7bef22a1774910dd4b7344191963b15564aa9a2e
sabergholami <72999667+sabergholami@users.noreply.github.com>
2022-08-10 12:19:13 -0400

    Create DeBruijn.py

---
+++ DeBruijn.py
@@ -0,0 +1,34 @@
+def DeBruijn(n):
+  import collections
+  def reverse_first_bit(b):
+    my_b = list(b)
+    if b[-1]=='0':
+      my_b[-1] = '1'
+      return ''.join(my_b)
+    else:
+      my_b[-1] = '0'
+      return ''.join(my_b)
+  
+  def left_shift(b):
+    x  = collections.deque(list(b))
+    x.rotate(-1)
+    return ''.join(x)
+
+
+
+  getbinary = lambda x, n: format(x, 'b').zfill(n)
+  G = nx.Graph()
+  for i in range(2**n):
+    node = int(str(getbinary(i,n)),2)
+    left = int(left_shift(getbinary(i,n)),2)
+    left_swap = int(reverse_first_bit(left_shift(getbinary(i,n))),2)
+    print (getbinary(i,n),left_shift(getbinary(i,n)),reverse_first_bit(left_shift(getbinary(i,n))))
+
+    # swap = int(str(reverse_last_bit(getbinary(i,n))),2)
+    # left = int(left_shift(getbinary(i,n)),2)
+    # right = int(left_shift(getbinary(i,n)),2)
+    if node!= left:
+      G.add_edge(node,left)
+    G.add_edge(node,left_swap)
+    # G.add_edge(node,right)
+  return G
